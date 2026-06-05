5fafde864efe2b2c1f4bd84b524cdd8fc5cdfd58
Juan dos Santos <juan13032007@gmail.com>
2026-06-05 00:20:05 -0300

    Etapa 2.2

---
+++ Fila.java
@@ -0,0 +1,50 @@
+public class Fila<T> {
+  private No<T> primeiro;
+  private No<T> ultimo;
+
+  public boolean filaVazia() {
+    return primeiro == null;
+  }
+
+  public void enfileira(T info) {
+    No<T> nv = new No<>(info);
+    if (filaVazia()) {
+      primeiro = nv;
+    } else {
+      ultimo.setProximo(nv);
+    }
+    ultimo = nv;
+  }
+
+  public T desenfileira() {
+    if (filaVazia()) throw new FilaVaziaException("Fila estava vazia");
+    T aux = ultimo.getInfo();
+    primeiro = primeiro.getProximo();
+    return aux;
+  }
+
+  public T primeiro() {
+    return primeiro.getInfo();
+  }
+
+  public int tamanho() {
+    int count = 0;
+    No<T> atual = primeiro;
+    while (atual != null) {
+      count++;
+      atual = atual.getProximo();
+    }
+    return count;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    No<T> atual = primeiro;
+    while (atual != null) {
+      sb.append(atual).append("->");
+    }
+    sb.append("\\");
+    return sb.toString();
+  }
+}
---
+++ FilaVaziaException.java
@@ -0,0 +1,6 @@
+public class FilaVaziaException extends RuntimeException {
+
+  public FilaVaziaException(String message) {
+    super(message);
+  }
+}
