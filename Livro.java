ba16284a1fae17d4091c5338e00c97b4c932fc76
Juan dos Santos <juan13032007@gmail.com>
2026-06-04 22:11:37 -0300

    Etapa 1.1
    Adiciona a classe Livro seguindo as especificação

---
+++ Livro.java
@@ -0,0 +1,64 @@
+public class Livro {
+  private String isbn;
+  private String titulo;
+  private String autor;
+  private int anoPub;
+  private boolean disponivel;
+
+  public Livro(String isbn, String titulo, String autor, int anoPub, boolean disponivel) {
+    this.isbn = isbn;
+    this.titulo = titulo;
+    this.autor = autor;
+    this.anoPub = anoPub;
+    this.disponivel = disponivel;
+  }
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public String getAutor() {
+    return autor;
+  }
+
+  public int getAnoPub() {
+    return anoPub;
+  }
+
+  public boolean isDisponivel() {
+    return disponivel;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public void setAutor(String autor) {
+    this.autor = autor;
+  }
+
+  public void setDisponivel(boolean disponivel) {
+    this.disponivel = disponivel;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("[")
+        .append(isbn)
+        .append("] ")
+        .append(titulo)
+        .append(" - ")
+        .append(autor)
+        .append("(")
+        .append(anoPub)
+        .append(") {}")
+        .append(disponivel ? "DISPONIVEL" : "INDISPONIVEL")
+        .append("}");
+    return sb.toString();
+  }
+}
